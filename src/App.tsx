a91bad7e1bc1e287bc440658d68cc44ef022ab13
Arun kumar <arunkumar562106@gmail.com>
2025-09-22 20:39:12 +0530

    Fix: Changed to HashRouter from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Dashboard from './pages/Dashboard';
 import OrderList from './pages/OrderList';
